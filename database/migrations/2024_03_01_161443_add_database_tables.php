54a2f639827415c88966c7d4fbad05020d212a12
DominikSommerTicketsprinter <dominik.sommer@ticketsprinter.de>
2024-03-01 18:31:59 +0100

    Refactor database migrations to remove unused columns

--- database/migrations/2024_03_01_161443_add_database_tables.php
+++ database/migrations/2024_03_01_161443_add_database_tables.php
@@ -16,21 +16,10 @@ return new class extends Migration
             $table->tinyInteger('type')->default(1);
             $table->string('title');
             $table->string('subTitle');
-            $table->tinyInteger('lottery')->default(0);
-            $table->integer('base_score')->default(10);
             $table->float('score')->default(10);
-            $table->smallInteger('genre_id');
             $table->text('description_1');
-            $table->text('description_2');
-            $table->text('description_3');
-            $table->text('key_facts');
-            $table->string('img1_url', 255);
-            $table->string('img1_source', 255);
             $table->tinyInteger('active');
-            $table->dateTime('published'); // Might need adjustment for actual use
             $table->date('date_activated')->nullable();
-            $table->string('thumbnail_url', 255);
-            $table->tinyInteger('on_page')->default(1);
             $table->mediumInteger('views')->default(0);
             $table->smallInteger('views_month')->comment('updated via cronjob');
             $table->smallInteger('views_week')->default(0)->comment('updated via cronjob');
@@ -39,30 +28,19 @@ return new class extends Migration
         });
         Schema::create('dates', function (Blueprint $table) {
             $table->integer('date_id')->primary();
-            $table->tinyInteger('type')->nullable();
             $table->integer('offer_id')->nullable();
-            $table->date('date');
-            $table->tinyInteger('period')->default(0);
             $table->date('period_start');
             $table->date('period_end');
             $table->smallInteger('eventlocation_id')->nullable();
-            $table->smallInteger('room_id');
             $table->time('start_time');
             $table->time('end_time');
             $table->date('event_start');
             $table->date('event_end');
-            $table->time('entry_time');
             $table->smallInteger('eventmanager_id')->nullable();
             $table->string('discount', 5);
             $table->string('discount_code', 200);
-            $table->dateTime('activation_datetime')->nullable();
-            $table->tinyInteger('hidden')->default(0);
-            $table->tinyInteger('disabled')->default(0);
-            $table->float('score')->default(10);
             $table->tinyInteger('sold_out')->default(0);
-            $table->tinyInteger('send_code')->default(0);
             $table->tinyInteger('active')->default(0);
-            $table->tinyInteger('frontend_status')->default(0);
         });
         Schema::create('offer_views', function (Blueprint $table) {
             $table->bigInteger('offer_view_id')->primary();
